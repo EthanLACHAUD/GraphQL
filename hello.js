5648a949180d66e5c8e61eb5f24a66b1452c1036
EthanLACHAUD <ethan.lachaud@etu.unilim.fr>
2024-03-14 10:10:00 +0100

    first api deployed and use

--- hello.js
+++ hello.js
@@ -1,15 +1,32 @@
-var { graphql, buildSchema } = require("graphql")
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
 
+// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `)
 
-var rootValue = { hello: () => "Hello world!" }
+// The root provides a resolver function for each API endpoint
+var root = {
+  rollDice: ({ numDice, numSides }) => {
+    var output = []
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
+  },
+}
 
-var source = "{ hello }"
-
-graphql({ schema, source, rootValue }).then(response => {
-  console.log(response)
-})
+var app = express()
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+)
+app.listen(4000)
+console.log("Running a GraphQL API server at localhost:4000/graphql")
